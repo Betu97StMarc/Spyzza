dcd5d8e70aaf4ae6cd3eea0f2efb20d31b89de15
RicardOP-gdev <61988635+RicardOP-gdev@users.noreply.github.com>
2020-05-14 13:20:29 +0200

    Blocking + Moviemiento de personaje empezado
    Se han puesto las animaciones básicas de movimiento al personaje principal en la escena Programming

--- Prototipe/Spyzza/Assets/Scenes/Programming/PlayerController/Scripts/PlayerController.cs
+++ Prototipe/Spyzza/Assets/Scenes/Programming/PlayerController/Scripts/PlayerController.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 1f;
-    bool canJump;
+    public bool canJump;
+    public bool isCrouched;    
     public Animator playerC;
 
 
@@ -24,24 +25,30 @@ public class PlayerController : MonoBehaviour
         float y = Input.GetAxisRaw("Vertical");
         playerC.SetFloat("Horizontal", x);
         playerC.SetFloat("Vertical", y);
+        playerC.SetFloat("Speed", speed);
 
-
-        if (Input.GetKey(KeyCode.W))
-        {
-            
+        
+       if (Input.GetKey(KeyCode.W))
+        {  
             transform.Translate(Vector3.forward * Time.deltaTime * speed);
-                   
-            if (Input.GetKey(KeyCode.LeftShift))
+            if(Input.GetKey(KeyCode.A) ||  Input.GetKey(KeyCode.D))
             {
-                speed = 0.5f;
-                transform.Translate(Vector3.forward * Time.deltaTime * speed);
+                speed = 1f;
             }
-            else
-            {
-                speed = 1;
-            }
-            
         }
+
+        if (Input.GetKey(KeyCode.LeftShift) && Input.GetKey(KeyCode.W))
+        {
+            speed = 2.5f;
+            transform.Translate(Vector3.forward * Time.deltaTime * speed);
+            Debug.Log("cacs");
+        }
+
+        else if (!Input.GetKey(KeyCode.LeftControl) && (!Input.GetKey(KeyCode.LeftShift)))
+        {
+            speed = 1.9f;
+        }
+
 
         if (Input.GetKey(KeyCode.S))
 
@@ -56,9 +63,34 @@ public class PlayerController : MonoBehaviour
         if (Input.GetButtonDown("Jump") && canJump)
         {
             canJump = false;
-            gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(0, 1000f, 0));
+            gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(0, 250f, 0));
+            playerC.Play("Jump");
         }
-             
+
+        if (Input.GetKey(KeyCode.LeftControl) && Input.GetKey(KeyCode.W))
+        {
+            speed = 0.8f;      
+        }
+        else if (!Input.GetKey(KeyCode.LeftControl) && (!Input.GetKey(KeyCode.LeftShift)))
+        {
+            speed = 1.9f;
+        }
+        /*if(Input.GetKey(KeyCode.LeftControl) && !isCrouched)
+        {
+            isCrouched = true;
+        }
+        if(Input.GetKeyUp(KeyCode.LeftControl) && isCrouched)
+        {
+            isCrouched = false;
+        }
+        if(isCrouched)
+        {
+            playerC.SetTrigger("Agacharse");
+        }
+        else
+        {
+            playerC.SetTrigger("Levantarse");
+        }*/
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -66,7 +98,6 @@ public class PlayerController : MonoBehaviour
         if (collision.transform.tag == "Ground")
         {
             canJump = true;
-
         }
     }
 }
